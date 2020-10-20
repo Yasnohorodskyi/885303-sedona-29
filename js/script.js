cc9182962a5aa5aa30a0026e601187d32593cab0
martinesidens <martines.idens@gmail.com>
2020-10-20 09:42:49 +0300

    20.10.2020 10:00

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 let popap = document.querySelector('.searche__form');
 let button = document.querySelector('.offer__button');
+let arrival = document.querySelector('.searche__form-arrival');
+let searcheForm = document.querySelector('.searche__form');
 
 button.addEventListener("click", function (evt) {
     if (popap.classList.contains("popap-show")) {
         evt.preventDefault();
         popap.classList.remove("popap-show");
     } else {
-        popap.classList.add("popap-show")
+        popap.classList.add("popap-show");
+        arrival.focus();
     }
 });
